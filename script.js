0cae3fcc5c5b63856fee845b1b01d66318dd2358
Vitaliy <skil.smile@gmail.com>
2017-12-04 19:22:39 +0200

    creating accordions

--- script.js
+++ script.js
@@ -2,15 +2,40 @@
 //section menu
 $('.kind__title').on('click', function() {
     var $kind = $(this).parent(),
+        $kindText = $kind.find('.kind__text'),
+        $kindTextInner = $kindText.find('.kind__text-inner'),
         activeClass = 'kind_active';
 
     if($kind.hasClass(activeClass)){
         $kind.removeClass(activeClass);
+
+        $kindTextInner.fadeOut(function(){
+            $kindText.css({
+                width:0
+            });
+        });
+
     } else {
         $kind.addClass(activeClass).siblings().removeClass(activeClass);
-    }
+        
+        $kindText.css({
+            width: Math.min(500, $('.menu').outerWidth()-$('.kind__title').length*$('.kind__title').outerWidth())
+        });
+
+
+        setTimeout(function(){
+            $kindTextInner.fadeIn();
+        },400);
+
+        $kind.siblings().find('.kind__text-inner').hide().parent().css({
+             width:0
+        });
+    };
     
+    
+
 });
+
 
 //section team
 
@@ -18,12 +43,12 @@ $('.team-acco__name').on('click', function() {
 
     let $teamAcco = $(this).parent(),
         activeClass = 'team-acco__item_active';
-
-    if($teamAcco.hasClass(activeClass)){
-        $teamAcco.removeClass(activeClass)
-    } else {
-        $teamAcco.addClass(activeClass).siblings().removeClass(activeClass);
+    
+    if(!$teamAcco.hasClass(activeClass)){
+        $teamAcco.siblings().removeClass(activeClass).find('.team-acco__content').slideUp();
     }
+    
+    $teamAcco.toggleClass(activeClass).find('.team-acco__content').slideToggle();
 
     return false;
 });
@@ -35,7 +60,7 @@ function initMap() {
     var uluru = {lat: 48.9215000, lng: 24.7097200};
     var map = new google.maps.Map(document.getElementById('mapq'), {
       zoom: 12,
-    //   scrollwheel: false,
+      scrollwheel: false,
       center: uluru
     });
     var marker = new google.maps.Marker({
